ed4e7e122d2e8266b80f6d0005909a7fb8ef09ef
onc157 <onc157@yandex.ru>
2020-09-30 22:36:52 +0300

    solved the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+  let result = 0;
+  if (array === ('' || undefined)) return 0;
+  array.sort(function(a, b) {
+    return a - b;
+  });
+
+  array.reverse().forEach(function(element, index, array) {
+    if (element < array[index - 1]) {
+      result = element
+    }
+  });
+  return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let result = 0;
+  if (array === ('' || undefined)) return 0;
+  array.sort(function(a, b) {
+    return a - b;
+  });
+
+  array.forEach(function(element, index, array) {
+    if (element > array[index - 1]) {
+      result = element
+    }
+  });
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let result = 0;
+  if ((!array) || (array.length === 0)) return 0;
+
+  array.forEach(function(element, index, array) {
+      result = result + +element.toFixed(2);
+  });
+
+  result = result / array.length;
+  return +result.toFixed(2);
 }
